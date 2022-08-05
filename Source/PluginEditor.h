908f15dc1e6d8a196f6eea3d7fc9692f7db38102
LAPTOP-6KKMLQ0I\Harvey <hfretwell@icloud.com>
2022-08-05 22:41:24 +0100

    Commiting source template

---
+++ Source/PluginEditor.h
@@ -0,0 +1,37 @@
+/*
+  ==============================================================================
+
+    PluginEditor.h
+    Created: 5 Aug 2022 10:05:54pm
+    Author:  Harvey Fretwell
+    
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class EQAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    EQAudioProcessorEditor (EQAudioProcessor&);
+    ~EQAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    EQAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (EQAudioProcessorEditor)
+};
